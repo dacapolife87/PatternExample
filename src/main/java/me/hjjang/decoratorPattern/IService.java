60ed9da30c03efdf3ac2088a363d4670febc998b
hJJANG <dacapolife87@gmail.com>
2021-02-01 07:50:21 +0900

    feat :  데코레이터 패턴 원래의 기능에 추가적인 기능을 부여할때 -> 그래서 데코레이터(장식)이라고한다. 인터페이스를 생성하여 데코레이터 안에서 원래의 기능을 호출하여 사용

---
+++ src/main/java/me/hjjang/decoratorPattern/IService.java
@@ -0,0 +1,5 @@
+package me.hjjang.decoratorPattern;
+
+public interface IService {
+    String runSomething();
+}
---
+++ src/main/java/me/hjjang/decoratorPattern/Service.java
@@ -0,0 +1,7 @@
+package me.hjjang.decoratorPattern;
+
+public class Service implements IService{
+    public String runSomething() {
+        return "서비스짱!!!";
+    }
+}
