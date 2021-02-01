e950cff1c26ac3b366ddc85c1fc30762ae037798
HyungjuJang <dacapolife87@gmail.com>
2021-02-01 20:14:04 +0900

    feat : 싱글턴 패턴

---
+++ src/main/java/me/hjjang/singleton/Client.java
@@ -0,0 +1,15 @@
+package me.hjjang.singleton;
+
+public class Client {
+    public static void main(String[] args) {
+//        Singleton singleton = new Singleton();
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+        System.out.println("s3 = " + s3);
+    }
+}
---
+++ src/main/java/me/hjjang/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package me.hjjang.singleton;
+
+public class Singleton {
+
+    static Singleton singletonObject;
+
+    private Singleton(){};
+
+    public static Singleton getInstance(){
+        if(singletonObject == null) {
+            singletonObject = new Singleton();
+        }
+
+        return singletonObject;
+    }
+}
